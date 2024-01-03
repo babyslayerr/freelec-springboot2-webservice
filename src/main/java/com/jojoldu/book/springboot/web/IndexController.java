28a0e4d821b5fd16c6072894ff4ea6743813131a
ì jjh619 <gksqlcxkdns1@naver.com>
2024-01-03 17:11:48 +0900

    * 등록화면 및 조회화면 수정, @Query어노테이션을 이용한 repository 조회 메소드 생성

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,14 +1,26 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RequiredArgsConstructor
+@Controller // 자동으로 컴포넌트 자동 등록
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         // ViewResolver가 경로 및 확장자 처리
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
